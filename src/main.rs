e21b93822df1e92ba58865393ab6217c746c0d41
wldmr <nope@wldmr.net>
2024-06-20 18:58:39 +0200

    Idea for modular rules. Added type-sitter generated code.
    As of this commit, needs a patched type-sitter-cli.
    It generated faulty code due to some name mangling
    regarding symbols. See https://github.com/Jakobeha/type-sitter/pull/4

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,11 @@
-use std::{io::Read, ops::Range};
+use std::io::Read;
 
-use tree_sitter::{Node, Parser, Query, QueryCursor, Tree};
+use tree_sitter::{InputEdit, Node, Parser, Point, Query, QueryCursor};
+use tree_sitter_ink::language as ink_lang;
+use type_sitter::ink::Knot;
+use type_sitter_lib::TypedNode;
+
+mod type_sitter;
 
 fn main() {
     let mut parser = Parser::new();
@@ -14,43 +19,166 @@ fn main() {
         .read_to_string(&mut source)
         .expect("Why can't we read from stdin?");
 
-    let tree = parser
+    let mut tree = parser
         .parse(&source, None)
         .expect("There should be a tree here.");
 
-    for (range, new_text) in edits(&tree, &source).into_iter().rev() {
-        source.replace_range(range, &new_text);
+    let rules: Vec<Box<dyn Rule>> = vec![Box::new(KnotEndMarkRule::new())];
+
+    for rule in rules {
+        for (range, new_text) in rule.edits(&tree.root_node(), &source).into_iter().rev() {
+            source.replace_range(range.start_byte..range.old_end_byte, &new_text);
+            tree.edit(&range);
+        }
+        tree = parser
+            .parse(&source, Some(&tree))
+            .expect("Parsing should work");
     }
 
     println!("{}", source);
 }
 
-fn edits(tree: &Tree, source: &str) -> Vec<(Range<usize>, String)> {
-    let query = Query::new(
-        &tree_sitter_ink::language(),
-        r#"
-        (knot name: (identifier) !end_mark) @knot_without_end
-        "#,
-    )
-    .expect("I wrote this, it must work.");
+trait Rule {
+    fn new() -> Self
+    where
+        Self: Sized;
+    fn edits(&self, node: &Node, source: &str) -> Vec<(InputEdit, String)>;
+}
 
-    let mut query_cursor = QueryCursor::new();
-    let kwe_index = query
-        .capture_index_for_name("knot_without_end")
-        .expect("I mean, just look up!") as usize;
-    let matches: Vec<Vec<Node>> = query_cursor
-        .captures(&query, tree.root_node(), source.as_bytes())
-        .filter(|&(_, i)| i == kwe_index)
-        .map(|it| it.0.captures.iter().map(|cap| cap.node).collect::<Vec<_>>())
-        .collect();
-    let mut result = Vec::new();
-    for nodes in matches {
-        for node in nodes {
-            let old_text = &source[node.byte_range()];
-            let new_text = old_text.to_string() + " ===";
-            let range = node.start_byte()..node.end_byte();
-            result.push((range, new_text));
+struct KnotEndMarkRule {
+    query: Query,
+    config: KnotRuleConfig,
+}
+
+struct KnotRuleConfig {
+    mark_size: usize,
+    closing_mark: bool,
+    mark_separator: String,
+}
+
+impl Default for KnotRuleConfig {
+    fn default() -> Self {
+        Self {
+            mark_size: 3,
+            closing_mark: true,
+            mark_separator: " ".to_string(),
         }
     }
-    result
 }
+
+impl Rule for KnotEndMarkRule {
+    fn new() -> Self {
+        Self {
+            query: Query::new(&ink_lang(), r#"(knot !function) @knot"#)
+                .expect("I wrote this, it must work."),
+            config: KnotRuleConfig::default(),
+        }
+    }
+
+    fn edits(&self, node: &Node, source: &str) -> Vec<(InputEdit, String)> {
+        let source = source.as_bytes();
+        let kwe_index = self
+            .query
+            .capture_index_for_name("knot")
+            .expect("I mean, just look up!") as usize;
+        let mut query_cursor = QueryCursor::new();
+        let knots = query_cursor
+            .captures(&self.query, *node, source)
+            .filter(|&(_, i)| i == kwe_index)
+            .flat_map(|(qmatch, _)| qmatch.captures.into_iter())
+            .filter_map(|cap| Knot::try_from(cap.node).ok())
+            .filter(|knot| !knot.has_error());
+
+        let end_marker = "=".repeat(self.config.mark_size);
+        knots
+            .filter_map(|knot| match knot.end_mark() {
+                None if self.config.closing_mark => {
+                    Some(edit(&knot, Edit::Append, vec![&end_marker]))
+                }
+                Some(Ok(mark))
+                    if mark
+                        .utf8_text(source)
+                        .is_ok_and(|it| it.len() != self.config.mark_size) =>
+                {
+                    Some(edit(&mark, Edit::Replace, vec![&end_marker]))
+                }
+                _ => None,
+            })
+            .collect()
+    }
+}
+
+enum Edit {
+    Append,
+    Prepend,
+    Replace,
+}
+
+fn edit<'a, T: TypedNode<'a>>(node: &T, edit: Edit, lines: Vec<&str>) -> (InputEdit, String) {
+    let lines: Vec<String> = lines.into_iter().map(|it| it.into()).collect();
+    let text = lines.join("\n");
+    let text_len = text.bytes().len();
+    let adjust_end_position = |it: &mut Point| {
+        it.row += lines.len().checked_sub(1).unwrap_or(0);
+        it.column = lines.last().map(String::len).unwrap_or(0);
+    };
+    let input_edit = match edit {
+        Edit::Append => InputEdit {
+            start_byte: node.end_byte(),
+            old_end_byte: node.end_byte(),
+            start_position: node.end_position(),
+            old_end_position: node.end_position(),
+
+            new_end_byte: node.end_byte() + text_len,
+            new_end_position: node.end_position().modify(adjust_end_position),
+        },
+        Edit::Prepend => InputEdit {
+            start_byte: node.start_byte(),
+            old_end_byte: node.start_byte(),
+            start_position: node.start_position(),
+            old_end_position: node.start_position(),
+
+            new_end_byte: node.start_byte() + text_len,
+            new_end_position: node.start_position().modify(adjust_end_position),
+        },
+        Edit::Replace => InputEdit {
+            start_byte: node.start_byte(),
+            old_end_byte: node.end_byte(),
+            start_position: node.start_position(),
+            old_end_position: node.end_position(),
+
+            new_end_byte: node.start_byte() + text_len,
+            new_end_position: node.start_position().modify(adjust_end_position),
+        },
+    };
+    (input_edit, text)
+}
+
+/// Implements Kotlin-like scope functions.
+trait ScopeFunc {
+    /// Modify in place
+    #[doc(alias = "also")]
+    #[doc(alias = "apply")]
+    fn modify<F>(mut self, f: F) -> Self
+    where
+        F: FnOnce(&mut Self),
+        Self: Sized,
+    {
+        f(&mut self);
+        self
+    }
+
+    /// Consuming map (can't use self afterwards)
+    #[doc(alias = "run")]
+    #[doc(alias = "let")]
+    #[doc(alias = "with")]
+    fn transform<F, T>(self, f: F) -> T
+    where
+        F: FnOnce(Self) -> T,
+        Self: Sized,
+    {
+        f(self)
+    }
+}
+
+impl<T> ScopeFunc for T {}
