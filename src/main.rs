ddec64e02266fbe0c29651752a724b48a0c90ac5
wldmr <nope@wldmr.net>
2024-07-01 08:49:23 +0200

    Main binary now also takes filenames

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,21 @@ use ink_fmt::{config::FormatConfig, format};
 
 fn main() {
     let mut source = String::new();
-    std::io::stdin()
-        .lock()
-        .read_to_string(&mut source)
-        .expect("Why can't we read from stdin?");
+    let args = &mut std::env::args();
+    let _ = args.next(); // that's us, we know who we are.
+    let filename = args.next();
+    if filename.is_none() || filename.as_ref().is_some_and(|it| it == "-") {
+        eprintln!("Reading from stdin");
+        std::io::stdin()
+            .lock()
+            .read_to_string(&mut source)
+            .expect("Why can't we read from stdin?");
+    } else {
+        eprintln!("Reading from file {}", filename.as_ref().unwrap());
+        source = std::fs::read_to_string(&filename.unwrap()).expect("File should exist");
+    }
 
+    assert!(!source.is_empty());
     let source = format(FormatConfig::default(), source);
 
     println!("{}", source);
