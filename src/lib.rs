3301909ea738305eec838da5b3a46ae2df624871
wldmr <nope@wldmr.net>
2024-07-01 11:56:53 +0200

    Somewhat improved performance and safety
    Performance: Apply as many non-overlapping edits
    as possible before reparsing.
    
    Safety: Abort if the source has (or is edited into)
    invalid syntax.

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,8 @@ pub mod config;
 pub mod edit;
 pub mod rules;
 
+use std::{fs::File, process::Command};
+
 use edit::Change;
 use rules::{init_rules, Rule};
 use tree_sitter::{Parser, Query, QueryCursor};
@@ -14,43 +16,96 @@ pub fn format(config: config::FormatConfig, mut source: String) -> String {
         .set_language(&tree_sitter_ink::language())
         .expect("We should be ablet to load an Ink grammar.");
 
-    let mut tree = parser
-        .parse(&source, None)
-        .expect("There should be a tree here.");
-
     let query = Query::new(&tree_sitter_ink::language(), QUERY).expect("query should be valid");
 
     let mut rules = init_rules(config, &query);
 
     let mut query_cursor = QueryCursor::new();
 
-    while let Some(Change { range, text }) =
-        next_edit(&mut query_cursor, &query, &tree, &source, &mut rules)
-    {
-        source.replace_range(range.start_byte..range.old_end_byte, &text);
-        tree.edit(&range);
+    let mut tree = parser
+        .parse(&source, None)
+        .expect("There should be a tree here.");
+
+    let mut edits = next_edits(&mut query_cursor, &query, &tree, &source, &mut rules);
+    let mut round = 0;
+    let mut edit_count = 0;
+    while !edits.is_empty() {
+        round += 1;
+        if tree.root_node().has_error() {
+            let graph = File::create("log.dot").expect("I should be able to create that file");
+            tree.print_dot_graph(&graph);
+            Command::new("dot")
+                .args(["-Tsvg", "-O", "log.dot"])
+                .output()
+                .expect("I should be able to call dot");
+
+            std::fs::write("error.fmt", &source).expect("I should be able to write that file");
+            panic!("Source can't be parsed. See log.dot.svg.\n{source}");
+        }
+        for Change { range, text } in edits.iter_mut().rev() {
+            edit_count += 1;
+            source.replace_range(range.start_byte..range.old_end_byte, &text);
+            tree.edit(range);
+        }
+
+        // eprintln!("After round {round}:\n{source}");
+
         tree = parser
             .parse(&source, Some(&tree))
-            .expect("Parsing should work, even if repeated.");
+            .expect("There should be a tree here.");
+        edits = next_edits(&mut query_cursor, &query, &tree, &source, &mut rules);
     }
 
+    eprintln!("Made {edit_count} edits in {round} rounds.");
     source
 }
 
-fn next_edit(
+fn next_edits(
     query_cursor: &mut QueryCursor,
     query: &Query,
     tree: &tree_sitter::Tree,
     source: &str,
     rules: &mut Vec<Box<dyn Rule>>,
-) -> Option<Change> {
+) -> Vec<Change> {
+    let mut edits = Vec::new();
     for match_ in query_cursor.matches(&query, tree.root_node(), source.as_bytes()) {
         for rule in rules.iter_mut() {
             let edit = rule.edit_if_needed(&query, &match_, source);
-            if edit.is_some() {
-                return edit;
+            if let Some(edit) = edit {
+                let prev = edits.last();
+                if prev.is_none()
+                    || prev.is_some_and(|prev: &Change| {
+                        edit.range.start_byte >= prev.range.old_end_byte
+                    })
+                {
+                    edits.push(edit);
+                }
             }
         }
     }
-    None
+    edits
+}
+
+#[cfg(test)]
+mod test {
+    use crate::{config::FormatConfig, format};
+
+    #[test]
+    fn test_replace() {
+        let input = "LIST lst = ( v1), v2";
+        let output = format(FormatConfig::default(), input.to_owned());
+        pretty_assertions::assert_str_eq!(output, "LIST lst = (v1), v2");
+    }
+
+    #[test]
+    fn test_indent_with() {
+        let input = r#"
+	I like {this|that|something else}.
+"#;
+        let expected = r#"
+I like {this|that|something else}.
+"#;
+        let output = format(FormatConfig::default(), input.to_owned());
+        pretty_assertions::assert_str_eq!(output, expected);
+    }
 }
