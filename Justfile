618d29e3f4abe56cb6f3eec8b541b93575405891
wldmr <nope@wldmr.net>
2024-09-23 13:53:33 +0200

    First stab at a command line interface

--- Justfile
+++ Justfile
@@ -12,10 +12,10 @@ diff target="examples/the_intercept.ink":
     echo -e "\n\n"
     {{diff}} "{{target}}" "$result"
 
-sample target="examples/the_intercept.ink":
+sample target="examples/input" output="examples/output":
     #!/usr/bin/env sh
-    samplyfile={{target}}.`git log -1 --format="%cI.%h"`.cargo_{{cargo_profile}}.geckoprofile.json
-    samply record --save-only -o $samplyfile -- cargo run --profile {{cargo_profile}} -- {{target}}
+    samplyfile={{target}}.sample.`git log -1 --format="%cI.%h"`.cargo_{{cargo_profile}}.geckoprofile.json
+    samply record --save-only -o $samplyfile -- cargo run --profile {{cargo_profile}} -- fmt {{target}} -o {{output}}
     # format the file
     jq . $samplyfile | sponge $samplyfile
     echo $samplyfile
--- tests/examples.rs
+++ tests/examples.rs
@@ -1,15 +1,15 @@
 #[test]
 fn the_intercept() {
-    let input = std::fs::read_to_string("examples/the_intercept.ink").unwrap();
-    let expected = std::fs::read_to_string("examples/the_intercept.fmt.ink").unwrap();
+    let input = std::fs::read_to_string("examples/input/the_intercept.ink").unwrap();
+    let expected = std::fs::read_to_string("examples/output/the_intercept.ink").unwrap();
     let output = ink_fmt::format(input);
     pretty_assertions::assert_str_eq!(output, expected);
 }
 
 #[test]
 fn emoji() {
-    let input = std::fs::read_to_string("examples/ld41-emoji.ink").unwrap();
-    let expected = std::fs::read_to_string("examples/ld41-emoji.fmt.ink").unwrap();
+    let input = std::fs::read_to_string("examples/input/ld41-emoji.ink").unwrap();
+    let expected = std::fs::read_to_string("examples/output/ld41-emoji.ink").unwrap();
     let output = ink_fmt::format(input);
     pretty_assertions::assert_str_eq!(output, expected);
 }
