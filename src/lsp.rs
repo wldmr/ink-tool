9845b3c7343cd6e21e2381b60ed793682d6477da
wldmr <nope@wldmr.net>
2024-10-09 11:40:34 +0200

    Support Notifications and thereby document sync

--- src/lsp.rs
+++ src/lsp.rs
@@ -1,22 +1,64 @@
-use clap::error::Result;
 use lsp_types::OneOf;
 use lsp_types::{InitializeParams, ServerCapabilities};
 
 use lsp_server::{
-    Connection, ExtractError, Message, ReqQueue, Request, RequestId, Response, ResponseError,
+    Connection, ExtractError, Message, Notification, Request, RequestId, Response, ResponseError,
 };
-use request::{DocumentSymbolRequest, HoverRequest};
+use notification::Notification as _;
+use tree_sitter::{InputEdit, Point};
 
 use std::collections::HashMap;
 
 use lsp_types::*;
 use type_sitter_lib::Node;
 
+use crate::AppResult;
+
 mod tree;
+
+pub struct Document {
+    tree: tree_sitter::Tree,
+    text: String,
+}
+
+impl Document {
+    pub fn new(parser: &mut tree_sitter::Parser) -> Self {
+        let text = String::new();
+        let tree = parser.parse(&text, None).expect("no reason to fail");
+        Self { tree, text }
+    }
+
+    fn byte_for(&self, pos: impl Into<lsp_types::Position>) -> Option<usize> {
+        let Position {
+            mut line,
+            mut character,
+        } = pos.into();
+        // Exceptionally stupid way to do it. TODO: Make more smart!
+        let mut prev_idx = 0;
+        for (idx, byte) in self.text.char_indices() {
+            let width = idx - prev_idx;
+            // eprintln!("{idx} - {line}:{character}");
+            prev_idx = idx;
+            if line > 0 {
+                if byte == '\n' {
+                    line -= 1
+                }
+            } else {
+                if character == 0 {
+                    return Some(idx);
+                } else {
+                    character -= width as u32;
+                }
+            }
+        }
+
+        None
+    }
+}
 
 pub struct LspState {
     parser: tree_sitter::Parser,
-    documents: HashMap<Uri, tree_sitter::Tree>,
+    documents: HashMap<Uri, Document>,
 }
 
 impl LspState {
@@ -32,21 +74,34 @@ impl LspState {
     }
 }
 
-use crate::AppResult;
-
 // *** Config Area: Register Server behaviors here ***
 
 fn server_capabilities() -> ServerCapabilities {
     ServerCapabilities {
         hover_provider: Some(HoverProviderCapability::Simple(true)),
         document_symbol_provider: Some(OneOf::Left(true)),
+        text_document_sync: Some(TextDocumentSyncCapability::Options(
+            TextDocumentSyncOptions {
+                open_close: Some(true),
+                change: Some(TextDocumentSyncKind::INCREMENTAL),
+                will_save: None,
+                will_save_wait_until: None,
+                save: None,
+            },
+        )),
         ..Default::default()
     }
 }
 
 static HANDLERS: &'static [RequestHandlerFn] = &[
-    HoverRequest::handle_request,
-    DocumentSymbolRequest::handle_request,
+    request::HoverRequest::handle_request,
+    request::DocumentSymbolRequest::handle_request,
+];
+
+static NOTIFICATION_HANDLERS: &'static [NotificationHandlerFn] = &[
+    notification::DidOpenTextDocument::handle_notification,
+    notification::DidCloseTextDocument::handle_notification,
+    notification::DidChangeTextDocument::handle_notification,
 ];
 
 enum RequestHandlerResult {
@@ -55,11 +110,17 @@ enum RequestHandlerResult {
     NotInterested(Request),
 }
 
+enum NotificationHandlerResult {
+    Success(Option<Notification>),
+    Failure(ResponseError),
+    NotInterested(Notification),
+}
+
 type RequestHandlerFn = fn(Request, &mut LspState) -> RequestHandlerResult;
+type NotificationHandlerFn = fn(Notification, &mut LspState) -> NotificationHandlerResult;
 
 pub fn run_lsp() -> AppResult<()> {
     // Note that  we must have our logging only write out to stderr.
-    eprintln!("starting generic LSP server");
 
     // Create the transport. Includes the stdio (stdin and stdout) versions but this could
     // also be implemented to use sockets or HTTP.
@@ -79,15 +140,14 @@ pub fn run_lsp() -> AppResult<()> {
     let _params: InitializeParams = serde_json::from_value(initialization_params).unwrap();
     let mut state = LspState::new();
 
-    eprintln!("starting example main loop");
     for msg in &connection.receiver {
-        eprintln!("got msg: {msg:?}");
+        // eprintln!("got msg: {}", serde_json::to_string_pretty(&msg).unwrap());
         match msg {
             Message::Request(req) => {
                 if connection.handle_shutdown(&req)? {
                     return Ok(());
                 }
-                eprintln!("got request: {req:?}");
+                // eprintln!("got request: {req:?}");
                 match handle_request(req, &mut state).into() {
                     Ok(response) => connection.sender.send(Message::Response(response))?,
                     Err(req) => eprintln!("unhandled request {req:?}"),
@@ -97,7 +157,14 @@ pub fn run_lsp() -> AppResult<()> {
                 eprintln!("got response: {resp:?}");
             }
             Message::Notification(not) => {
-                eprintln!("got notification: {not:?}");
+                // eprintln!("got notification: {not:?}");
+                match handle_notification(not, &mut state) {
+                    Ok(None) => {}
+                    Ok(Some(response)) => {
+                        connection.sender.send(Message::Notification(response))?;
+                    }
+                    Err(not) => eprintln!("unhandled notification {not:?}"),
+                }
             }
         }
     }
@@ -132,36 +199,71 @@ fn handle_request(mut request: Request, state: &mut LspState) -> Result<Response
     Err(request)
 }
 
-trait RequestHandler: lsp_types::request::Request {
-    fn execute(params: Self::Params, state: &mut LspState) -> Self::Result;
+fn handle_notification(
+    mut notification: Notification,
+    state: &mut LspState,
+) -> Result<Option<Notification>, Notification> {
+    for handler in NOTIFICATION_HANDLERS {
+        use NotificationHandlerResult::*;
+        match handler(notification, state) {
+            NotInterested(it) => notification = it, // let the next handler try
+            Success(reply) => {
+                return Ok(reply);
+            }
+            Failure(err) => {
+                return Ok(Some(lsp_server::Notification::new(
+                    lsp_types::notification::LogMessage::METHOD.to_owned(),
+                    err,
+                )));
+            }
+        }
+    }
+    Err(notification)
+}
+
+trait RequestHandler: lsp_types::request::Request
+where
+    Self::Params: std::fmt::Debug,
+{
+    fn execute(params: Self::Params, state: &mut LspState) -> Result<Self::Result, ResponseError>;
 
     fn handle_request(req: Request, state: &mut LspState) -> RequestHandlerResult {
+        use RequestHandlerResult::*;
         match req.extract(Self::METHOD) {
-            Ok((id, params)) => {
-                // eprintln!("got gotoDefinition request #{id}: {params:?}");
-                let result = Self::execute(params, state);
-                let result = serde_json::to_value(&result).unwrap();
-                RequestHandlerResult::Success(id, result)
-            }
+            Ok((id, params)) => match Self::execute(params, state) {
+                Ok(result) => {
+                    let result = serde_json::to_value(&result).unwrap();
+                    Success(id, result)
+                }
+                Err(error) => Failure(id, error),
+            },
             Err(err @ ExtractError::JsonError { .. }) => panic!("{err:?}"), // maybe we should skip malformed json gracefully?
-            Err(ExtractError::MethodMismatch(req)) => RequestHandlerResult::NotInterested(req),
+            Err(ExtractError::MethodMismatch(req)) => NotInterested(req),
         }
     }
 }
 
-impl RequestHandler for HoverRequest {
-    fn execute(_params: Self::Params, _state: &mut LspState) -> Self::Result {
-        Some(Hover {
+impl RequestHandler for lsp_types::request::HoverRequest {
+    fn execute(
+        _params: Self::Params,
+        _state: &mut LspState,
+    ) -> Result<Option<lsp_types::Hover>, ResponseError> {
+        Ok(Some(Hover {
             contents: HoverContents::Scalar(MarkedString::String(
                 "You are indeed hovering".to_owned(),
             )),
             range: None,
-        })
+        }))
     }
 }
 
-impl RequestHandler for DocumentSymbolRequest {
-    fn execute(params: Self::Params, _state: &mut LspState) -> Self::Result {
+impl RequestHandler for lsp_types::request::DocumentSymbolRequest {
+    fn execute(
+        params: Self::Params,
+        _state: &mut LspState,
+    ) -> Result<Option<lsp_types::DocumentSymbolResponse>, ResponseError> {
+        #[allow(deprecated)]
+        // `deprecated` is deprecated (ironic). But since we can't _not_ use it when constructing a value, we need to shut up the warnings here
         let info = SymbolInformation {
             name: "Boo".to_owned(),
             kind: SymbolKind::FIELD,
@@ -173,7 +275,136 @@ impl RequestHandler for DocumentSymbolRequest {
             },
             container_name: None,
         };
-        Some(DocumentSymbolResponse::Flat(vec![info]))
+        Ok(Some(DocumentSymbolResponse::Flat(vec![info])))
+    }
+}
+
+trait NotificationHandler: lsp_types::notification::Notification
+where
+    Self::Params: std::fmt::Debug,
+{
+    fn execute(
+        params: Self::Params,
+        state: &mut LspState,
+    ) -> Result<Option<Notification>, ResponseError>;
+
+    fn handle_notification(
+        notification: lsp_server::Notification,
+        state: &mut LspState,
+    ) -> NotificationHandlerResult {
+        use NotificationHandlerResult::*;
+        match notification.extract(Self::METHOD) {
+            Ok(params) => match Self::execute(params, state) {
+                Ok(reply) => Success(reply),
+                Err(err) => Failure(err),
+            },
+            Err(err @ ExtractError::JsonError { .. }) => panic!("{err:?}"), // maybe we should skip malformed json gracefully?
+            Err(ExtractError::MethodMismatch(req)) => NotInterested(req),
+        }
+    }
+}
+
+impl NotificationHandler for lsp_types::notification::DidOpenTextDocument {
+    fn execute(
+        params: Self::Params,
+        state: &mut LspState,
+    ) -> Result<Option<Notification>, ResponseError> {
+        let text = params.text_document.text;
+        let tree = state
+            .parser
+            .parse(&text, None)
+            .expect("parsing should always work");
+        let old = state
+            .documents
+            .insert(params.text_document.uri, Document { tree, text });
+        assert!(old.is_none());
+        Ok(None)
+    }
+}
+
+impl NotificationHandler for lsp_types::notification::DidCloseTextDocument {
+    fn execute(
+        params: Self::Params,
+        state: &mut LspState,
+    ) -> Result<Option<Notification>, ResponseError> {
+        let old = state.documents.remove(&params.text_document.uri);
+        assert!(old.is_some());
+        Ok(None)
+    }
+}
+
+impl NotificationHandler for lsp_types::notification::DidChangeTextDocument {
+    fn execute(
+        params: Self::Params,
+        state: &mut LspState,
+    ) -> Result<Option<Notification>, ResponseError> {
+        let doc = state
+            .documents
+            .get_mut(&params.text_document.uri)
+            .expect("we should have put it in there with during didOpen");
+        for change in params.content_changes {
+            let edit = change
+                .range
+                .map(|range| edit_range(&doc, range, &change.text))
+                .unwrap_or_else(|| edit_whole_document(&doc, &change.text));
+            doc.tree.edit(&edit);
+            doc.text
+                .replace_range(edit.start_byte..edit.old_end_byte, &change.text);
+        }
+        doc.tree = state
+            .parser
+            .parse(&doc.text, Some(&doc.tree))
+            .expect("parsing must work");
+        Ok(None)
+    }
+}
+
+fn point(pos: &Position) -> tree_sitter::Point {
+    tree_sitter::Point {
+        row: pos.line as usize,
+        column: pos.character as usize,
+    }
+}
+
+fn edit_range(doc: &Document, range: Range, new_text: &str) -> tree_sitter::InputEdit {
+    let start_byte = doc
+        .byte_for(range.start)
+        .expect("range must be within document");
+    let old_end_byte = doc
+        .byte_for(range.end)
+        .expect("range must be within document");
+    let new_end_byte = start_byte + new_text.bytes().len();
+
+    let start_position = point(&range.start);
+    let old_end_position = point(&range.end);
+    let mut new_end_position = start_position.clone();
+    for char in new_text.chars() {
+        if char == '\n' {
+            new_end_position.row += 1;
+            new_end_position.column = 0;
+        } else {
+            new_end_position.column += 1;
+        }
+    }
+
+    tree_sitter::InputEdit {
+        start_byte,
+        old_end_byte,
+        new_end_byte,
+        start_position,
+        old_end_position,
+        new_end_position,
+    }
+}
+
+fn edit_whole_document(doc: &Document, new_text: &str) -> InputEdit {
+    InputEdit {
+        start_byte: 0,
+        old_end_byte: doc.text.len(),
+        new_end_byte: new_text.len(),
+        start_position: Point::new(0, 0),
+        old_end_position: Point::new(0, 0),
+        new_end_position: Point::new(0, 0),
     }
 }
 
