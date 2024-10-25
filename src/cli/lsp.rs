cf844f01e0021bcfc7758b180f476afb9bea55ce
wldmr <nope@wldmr.net>
2024-10-25 08:51:31 +0200

    Spec-compliant `--stdio` CLI switch (currently a no-op)
    VSCode starts language servers like this,
    so we, provide the switch for compatibility.

--- src/cli/lsp.rs
+++ src/cli/lsp.rs
@@ -2,7 +2,19 @@ use clap::Args;
 use ink_tool::{lsp::run_lsp, AppResult};
 
 #[derive(Args, Debug)]
-pub(crate) struct LspOpt;
+#[group(required = false, multiple = false)]
+/// Spec-compliant arguments for LSP communication channel
+pub(crate) struct Communication {
+    /// Communicate over stdin/stdout (currently the only option)
+    #[arg(long)]
+    stdio: bool,
+}
+
+#[derive(Args, Debug)]
+pub(crate) struct LspOpt {
+    #[command(flatten)]
+    communication: Communication,
+}
 
 pub(crate) fn lsp(_opt: LspOpt) -> AppResult<()> {
     run_lsp()
