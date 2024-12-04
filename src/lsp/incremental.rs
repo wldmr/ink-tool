bc28b4a268dfb8a9517b9381f0d821cb0da8a639
wldmr <nope@wldmr.net>
2024-12-04 19:24:50 +0100

    Nice way of building computation nodes
    Previously, we had to give two functions: One to specify keys
    for the inputs and one to do a calculation on the inputs. Now
    we have combinators to build computations with where you specify
    a key and a computation step on the value of that key right away.
    
    This _looks_ a bit nicer, but that means we can no longer have
    a variable number of inputs. Another goal was to be able to
    conditionaly include or remove values, which we also haven't
    achieved. Maybe a new combinator?
    
    But really, what I want is a way to "instrument" functions
    so that the computations are only built from them *after*
    all relevant keys have been registered. No clue how to
    achieve that yet.

--- src/lsp/incremental.rs
+++ src/lsp/incremental.rs
@@ -1,5 +1,6 @@
-///! Very basic incremental computation facilities, based on this article:
-///! <https://medium.com/@eliah.lakhin/salsa-algorithm-explained-c5d6df1dd291>
+///! Very basic incremental computation facilities, based on the following articles:
+///! * [Salsa Algorithm Explained](https://medium.com/@eliah.lakhin/salsa-algorithm-explained-c5d6df1dd291)
+///! * [How to recalculate a Spreadsheet]<https://lord.io/spreadsheets/>
 use std::{
     collections::HashMap,
     fmt::Debug,
@@ -14,19 +15,85 @@ impl Revision {
     }
 }
 
-struct Cell<K, V> {
-    updated_at: Revision,
-    verified_at: Revision,
-    cache: Option<(u64, V)>,
-    /// Which keys does this cell depend on?
-    inputs: fn() -> Vec<K>,
-    /// Compute the value given all the input values (corresponding to the result of `inputs`)
-    compute: fn(Vec<V>) -> V,
+#[derive(Clone, Debug)]
+enum Computation<K, V> {
+    Const {
+        value: V,
+        next: Option<Box<ComputationStep<K, V>>>,
+    },
+    Derive {
+        input: K,
+        derive: fn(&V) -> V,
+        next: Option<Box<ComputationStep<K, V>>>,
+    },
 }
 
+#[derive(Clone, Debug)]
+struct ComputationStep<K, V> {
+    input: K,
+    /// Function from (previous output, value for input) to new output
+    combine: fn(&V, &V) -> V,
+    next: Option<Box<Self>>,
+}
+
+impl<K: Key, V: Value> Computation<K, V> {
+    /// A computation (after a fashion) that simply returns `value`
+    pub(crate) fn constant(value: V) -> Self {
+        Self::Const { value, next: None }
+    }
+
+    /// Convenience function to a computation that simply contains the value behind `input`.
+    pub(crate) fn fetch(input: K) -> Self {
+        Self::derive(input, |value| value.clone())
+    }
+
+    /// Convenience function to a computation that simply contains the value behind `input`.
+    pub(crate) fn derive(input: K, derive: fn(&V) -> V) -> Self {
+        Self::Derive {
+            input,
+            derive,
+            next: None,
+        }
+    }
+
+    /// Combines the result of the previous step and the value behind `input` using the `combine` operation.
+    pub(crate) fn and_combine(mut self, input: K, combine: fn(&V, &V) -> V) -> Self {
+        match &mut self {
+            Computation::Const { next, .. } | Computation::Derive { next, .. } => {
+                let new_step = ComputationStep {
+                    input,
+                    combine,
+                    next: None,
+                };
+                if let Some(step) = next {
+                    step.set_next(new_step);
+                } else {
+                    *next = Some(Box::new(new_step));
+                }
+            }
+        }
+        self
+    }
+}
+
+impl<K, V> ComputationStep<K, V> {
+    // Helper function: Find the last element in the computation chain and add `step`
+    fn set_next(&mut self, step: Self) {
+        if let Some(ref mut inner) = self.next {
+            inner.set_next(step);
+        } else {
+            self.next = Some(Box::new(step));
+        }
+    }
+}
+
+#[derive(Debug)]
 pub(crate) struct Db<K, V> {
     revision: Revision,
-    cells: HashMap<K, Cell<K, V>>,
+    cache: HashMap<K, (u64, V)>,
+    computation: HashMap<K, Computation<K, V>>,
+    updated_at: HashMap<K, Revision>,
+    verified_at: HashMap<K, Revision>,
 }
 
 trait Key: Hash + Eq + Copy + Debug {}
@@ -39,112 +106,73 @@ impl<K: Key, V: Value> Db<K, V> {
     pub(crate) fn new() -> Self {
         Self {
             revision: Revision(0),
-            cells: HashMap::new(),
+            cache: HashMap::new(),
+            computation: HashMap::new(),
+            updated_at: HashMap::new(),
+            verified_at: HashMap::new(),
         }
     }
 
-    pub(crate) fn value(&mut self, key: K) -> V {
+    /// Get the current value behind `key`, if any.
+    pub(crate) fn value(&mut self, key: K) -> Option<&V> {
         self.verify(key);
-        self.cells
-            .get(&key)
-            .expect("algorithm must guarantee that verified cache is filled")
-            .cache
-            .clone()
-            .unwrap()
-            .1
+        self.cache.get(&key).map(|it| &it.1)
     }
 
     /// Verify and update cell behind `key`. Return the revision of the latest update
-    /// (which may be the current revision, if any input has changed (recursively)).
-    fn verify(&mut self, key: K) -> Revision {
-        let cell_verified_at = self
-            .cells
-            .get(&key)
-            .expect("mustn't get cells that don't exists")
-            .verified_at;
-        if cell_verified_at == self.revision {
-            // eprintln!("verify {key:?}: verified == {:?}", self.revision);
-            return self
-                .cells
-                .get(&key)
-                .expect("mustn't get cells that don't exists")
-                .updated_at;
-        }
-
-        if self.cells.get(&key).unwrap().cache.is_none() {
-            // eprintln!("verify {key:?}: empty cache -> recompute");
-            let input_keys = (self.cells.get(&key).unwrap().inputs)();
-            let input_values = input_keys
-                .iter()
-                .map(|key| self.value(*key).clone())
-                .collect();
-            let cell = self.cells.get_mut(&key).unwrap();
-            let new_value = (cell.compute)(input_values);
-            let new_hash = hash(&new_value);
-            cell.cache = Some((new_hash, new_value));
-            cell.updated_at = self.revision;
-            cell.verified_at = self.revision;
-            return self.revision;
+    /// (which may be the current revision, if any input has changed (recursively),
+    /// or an earlier one, if no update was required).
+    fn verify(&mut self, key: K) -> Option<&Revision> {
+        // We're not really doing anything useful with the `Option` return value, but it enables the early return, so there's that.
+        let current_cell_verified_at = *self.verified_at.get(&key)?;
+        if current_cell_verified_at == self.revision {
+            // eprintln!("verify {key:?}: up to date at {:?}", self.revision);
+            return self.updated_at.get(&key);
         }
 
         // Couldn't shortcut, so: Verify/update all inputs, …
-        let mut encountered_newer_input = false;
-        for input_key in self.input_keys_for(&key) {
-            let is_newer = self.verify(input_key) > cell_verified_at;
-            // eprintln!("verify {key:?}: input {input_key:?} newer? {is_newer}");
-            encountered_newer_input |= is_newer;
-        }
-
-        // … and recompute current cell if any input cell was newer than this cell.
-        if encountered_newer_input {
-            // eprintln!("verify {key:?}: outated because some input is newer -> recompute");
-            let input_values = self
-                .input_keys_for(&key)
-                .iter()
-                .map(|input_key| self.cells.get(input_key).unwrap().cache.clone().unwrap().1)
-                .collect();
-            let cell = self.cells.get_mut(&key).unwrap();
-            let new_value = (cell.compute)(input_values);
-            let new_hash = hash(&new_value);
-            let old_hash = cell.cache.as_ref().unwrap().0;
-            if new_hash == old_hash {
-                //eprintln!("verify {key:?}: hash hasn't changed -> set verified_at to {:?}", self.revision);
-                cell.verified_at = self.revision;
-                cell.updated_at
-            } else {
-                // eprintln!("verify {key:?}: hash has changed -> insert new value set revisions to {:?}", self.revision);
-                cell.cache = Some((new_hash, new_value));
-                cell.updated_at = self.revision;
-                cell.verified_at = self.revision;
-                cell.updated_at
+        let (mut accumulator, mut next) = match self.computation.get(&key).cloned()? {
+            Computation::Const { value, next } => (value.clone(), next),
+            Computation::Derive {
+                input,
+                derive,
+                next,
+            } => {
+                let input_value = self.value(input)?;
+                (derive(input_value), next)
             }
-        } else {
-            let cell = self.cells.get_mut(&key).unwrap();
-            cell.verified_at = self.revision;
-            cell.updated_at
+        };
+        while let Some(ref step) = next {
+            let input_value = self.value(step.input)?;
+            accumulator = (step.combine)(&accumulator, input_value);
+            next = step.next.clone();
         }
+
+        let new_hash = hash(&accumulator);
+        if self.cache.get(&key).is_some_and(|it| it.0 == new_hash) {
+            //eprintln!("verify {key:?}: hash hasn't changed -> set verified_at to {:?}", self.revision);
+            self.verified_at.insert(key, self.revision);
+        } else {
+            // eprintln!("verify {key:?}: hash has changed -> insert new value set revisions to {:?}", self.revision);
+            self.cache.insert(key, (new_hash, accumulator));
+            self.verified_at.insert(key, self.revision);
+            self.updated_at.insert(key, self.revision);
+        }
+        self.updated_at.get(&key)
     }
 
-    fn input_keys_for(&self, key: &K) -> Vec<K> {
-        (self.cells.get(key).unwrap().inputs)()
+    pub(crate) fn set_value(&mut self, key: K, computation: impl Into<Computation<K, V>>) {
+        self.computation.insert(key, computation.into());
+        self.verified_at.insert(key, self.revision);
+        self.updated_at.insert(key, self.revision);
+        self.revision.inc(); // Increase _after_ setting the timestamps so that the values are calculated when next observed.
     }
+}
 
-    pub(crate) fn set_input(&mut self, key: K, compute: fn(Vec<V>) -> V) {
-        self.set_derived(key, Vec::new, compute)
-    }
-
-    pub(crate) fn set_derived(&mut self, key: K, inputs: fn() -> Vec<K>, compute: fn(Vec<V>) -> V) {
-        self.cells.insert(
-            key,
-            Cell {
-                updated_at: self.revision,
-                verified_at: self.revision,
-                cache: None,
-                inputs,
-                compute,
-            },
-        );
-        self.revision.inc();
+/// Create a constant computation from a value
+impl<K: Key, V: Value> From<V> for Computation<K, V> {
+    fn from(value: V) -> Self {
+        Computation::Const { value, next: None }
     }
 }
 
@@ -161,35 +189,39 @@ mod tests {
 
     /// Direct translation of the example in <https://medium.com/@eliah.lakhin/salsa-algorithm-explained-c5d6df1dd291>
     #[test]
-    fn example_spreadsheet() {
+    fn spreadsheet() {
         #[derive(Debug, Hash, PartialEq, Eq, Clone, Copy)]
         #[rustfmt::skip]
         enum Keys { A, B, C, D }
 
-        let mut db = Db::new();
-        db.set_input(Keys::A, |_| 10);
-        db.set_input(Keys::B, |_| 20);
-        db.set_derived(Keys::C, || vec![Keys::A], |v| v[0] + 5);
-        db.set_derived(Keys::D, || vec![Keys::B, Keys::C], |v| v[0] + v[1]);
-        assert_eq!(db.value(Keys::D), 35);
+        let mut db: Db<Keys, i32> = Db::new();
+        db.set_value(Keys::A, 10);
+        db.set_value(Keys::B, 20);
+        db.set_value(Keys::C, Computation::derive(Keys::A, |a| a + 5));
+        db.set_value(
+            Keys::D,
+            Computation::fetch(Keys::B).and_combine(Keys::C, |b, c| b + c),
+        );
+        eprintln!("{db:#?}");
+        assert_eq!(db.value(Keys::D), Some(&35));
 
-        db.set_input(Keys::B, |_| 23);
-        assert_eq!(db.value(Keys::D), 38);
+        db.set_value(Keys::B, 23);
+        assert_eq!(db.value(Keys::D), Some(&38));
 
-        db.set_input(Keys::B, |_| 23);
-        assert_eq!(db.value(Keys::D), 38);
+        db.set_value(Keys::B, 23);
+        assert_eq!(db.value(Keys::D), Some(&38));
 
-        db.set_input(Keys::A, |_| 11);
-        assert_eq!(db.value(Keys::D), 39);
+        db.set_value(Keys::A, 11);
+        assert_eq!(db.value(Keys::D), Some(&39));
 
-        db.set_input(Keys::A, |_| 11);
-        assert_eq!(db.value(Keys::D), 39);
+        db.set_value(Keys::A, 11);
+        assert_eq!(db.value(Keys::D), Some(&39));
     }
 
     /// Direct translation of the example in <https://lord.io/spreadsheets/>
     /// Since this is a more elaborate example, we can whitebox-test which cells are actually touched for an update.
     #[test]
-    fn example_burrito() {
+    fn burrito() {
         #[derive(Debug, Hash, PartialEq, Eq, Clone, Copy)]
         enum Keys {
             BurritoPrice,
@@ -233,57 +265,54 @@ mod tests {
         }
 
         let mut db = Db::new();
-        db.set_input(Keys::BurritoPrice, |_| Values::Dollars(8));
-        db.set_input(Keys::NumBurritos, |_| Values::Amount(3));
-        db.set_input(Keys::SalsaPerBurrito, |_| Values::Grams(40));
-        db.set_derived(
+        db.set_value(Keys::BurritoPrice, Values::Dollars(8));
+        db.set_value(Keys::NumBurritos, Values::Amount(3));
+        db.set_value(Keys::SalsaPerBurrito, Values::Grams(40));
+        db.set_value(
             Keys::BurritoPriceWithShip,
-            || vec![Keys::BurritoPrice],
-            |v| v[0] + Values::Dollars(2),
+            Computation::derive(Keys::BurritoPrice, |&price| price + Values::Dollars(2)),
         );
-        db.set_derived(
+        db.set_value(
             Keys::Total,
-            || vec![Keys::BurritoPriceWithShip, Keys::NumBurritos],
-            |v| v[0] * v[1],
+            Computation::fetch(Keys::BurritoPriceWithShip)
+                .and_combine(Keys::NumBurritos, |&price, &num| price * num),
         );
-        db.set_derived(
+        db.set_value(
             Keys::SalsaInOrder,
-            || vec![Keys::NumBurritos, Keys::SalsaPerBurrito],
-            |v| v[0] * v[1],
+            Computation::fetch(Keys::NumBurritos)
+                .and_combine(Keys::SalsaPerBurrito, |&num, &salsa| num * salsa),
         );
         let revision_1 = db.revision;
 
-        assert_eq!(db.value(Keys::Total), Values::Dollars(30));
-        assert_eq!(db.value(Keys::SalsaInOrder), Values::Grams(120));
-        assert!(db.cells.values().all(|it| it.updated_at == revision_1));
-        assert!(db.cells.values().all(|it| it.verified_at == revision_1));
+        assert_eq!(db.value(Keys::Total), Some(&Values::Dollars(30)));
+        assert_eq!(db.value(Keys::SalsaInOrder), Some(&Values::Grams(120)));
+        assert!(db.updated_at.values().all(|&it| it == revision_1));
+        assert!(db.verified_at.values().all(|&it| it == revision_1));
 
-        db.set_input(Keys::BurritoPrice, |_| Values::Dollars(9));
-        db.set_derived(
+        db.set_value(Keys::BurritoPrice, Values::Dollars(9));
+        db.set_value(
             Keys::Total,
-            || vec![Keys::BurritoPriceWithShip, Keys::NumBurritos],
-            |v| (v[0] * v[1]) + Values::Dollars(2),
+            Computation::fetch(Keys::BurritoPriceWithShip)
+                .and_combine(Keys::NumBurritos, |&price, &num| {
+                    (price * num) + Values::Dollars(2)
+                }),
         );
         let revision_2 = db.revision;
 
-        assert_eq!(db.value(Keys::Total), Values::Dollars(35));
+        assert_eq!(db.value(Keys::Total), Some(&Values::Dollars(35)));
         assert!(
-            db.cells.values().any(|it| it.verified_at == revision_2)
-                && !db.cells.values().all(|it| it.verified_at == revision_2),
+            db.verified_at.values().any(|&it| it== revision_2)
+                && !db.verified_at.values().all(|&it| it== revision_2),
             "some (but not all) are verified at revision 2 because we haven't observed SalsaInOrder yet."
         );
-        assert_eq!(db.value(Keys::SalsaInOrder), Values::Grams(120));
+        assert_eq!(db.value(Keys::SalsaInOrder), Some(&Values::Grams(120)));
         assert!(
-            db.cells.values().all(|it| it.verified_at == revision_2),
+            db.verified_at.values().all(|&it| it == revision_2),
             "now all are verified at revision 2, because we *have* observed all output cells."
         );
+        assert_eq!(db.updated_at[&Keys::Total], revision_2, "total was updated");
         assert_eq!(
-            db.cells[&Keys::Total].updated_at,
-            revision_2,
-            "total was updated"
-        );
-        assert_eq!(
-            db.cells[&Keys::SalsaInOrder].updated_at,
+            db.updated_at[&Keys::SalsaInOrder],
             revision_1,
             "salsa in order remains untouched"
         );
