1f7c9c94f482272904d91bad2c0aeec98ce08657
wldmr <nope@wldmr.net>
2025-02-07 13:01:07 +0100

    Logging now configured through cli arguments
    I didn't want to spam the file system with
    additional files that don't get cleaned up.
    
    This way, users can decide to write logs to
    a file if the want. Default operation is to
    write the logs to stderr, which means they'll
    get sent to the lsp client in lsp mode.

--- src/cli.rs
+++ src/cli.rs
@@ -1,5 +1,6 @@
 use clap::{command, Parser, Subcommand};
 use ink_tool::AppResult;
+use std::path::PathBuf;
 
 pub(crate) mod fmt;
 pub(crate) mod lsp;
@@ -9,6 +10,32 @@ pub(crate) mod lsp;
 struct Cli {
     #[command(subcommand)]
     command: Commands,
+    /// log to this file instead of stderr
+    #[arg(long)]
+    log_file: Option<PathBuf>,
+    /// minimum log level
+    #[arg(long, value_enum, default_value_t)]
+    log_level: LogLevel,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, clap::ValueEnum)]
+enum LogLevel {
+    Off,
+    Error,
+    Warn,
+    Info,
+    Debug,
+    Trace,
+}
+
+impl Default for LogLevel {
+    fn default() -> Self {
+        if cfg!(debug_assertions) {
+            LogLevel::Debug
+        } else {
+            LogLevel::Info
+        }
+    }
 }
 
 #[derive(Subcommand, Debug)]
@@ -19,20 +46,41 @@ enum Commands {
 
 pub(crate) fn main() -> AppResult<()> {
     let args = Cli::parse();
-    let proj = directories::ProjectDirs::from("net", "wldmr", "ink-tool")
-        .ok_or("Couldn't find required directories")?;
-    let state_dir = proj.state_dir().unwrap_or_else(|| proj.cache_dir());
-    std::fs::create_dir_all(state_dir)?;
-    let log_file = state_dir.join("ink-tool.log");
-    let log_file = log_file
-        .to_str()
-        .ok_or_else(|| format!("Couldn't create log file `{log_file:?}`"))?;
-    ftail::Ftail::new()
-        .single_file(log_file, true, log::LevelFilter::max())
-        .max_file_size(5)
-        .init()?;
+    setup_logging(&args)?;
     match args.command {
         Commands::Fmt(opt) => fmt::fmt(opt),
         Commands::Lsp(opt) => lsp::lsp(opt),
     }
+}
+
+fn setup_logging(args: &Cli) -> AppResult<()> {
+    let level = match args.log_level {
+        LogLevel::Off => log::LevelFilter::Off,
+        LogLevel::Error => log::LevelFilter::Error,
+        LogLevel::Warn => log::LevelFilter::Warn,
+        LogLevel::Info => log::LevelFilter::Info,
+        LogLevel::Debug => log::LevelFilter::Debug,
+        LogLevel::Trace => log::LevelFilter::Trace,
+    };
+    let mut dispatch = fern::Dispatch::new()
+        .level(log::LevelFilter::Error)
+        .level_for("ink_tool", level)
+        .format(move |out, message, record| {
+            out.finish(format_args!(
+                "{} {} {} {}",
+                humantime::format_rfc3339_seconds(std::time::SystemTime::now()),
+                record.level(),
+                record.target(),
+                message
+            ))
+        });
+
+    if let Some(ref log_file) = args.log_file {
+        dispatch = dispatch.chain(fern::log_file(log_file)?)
+    } else {
+        dispatch = dispatch.chain(std::io::stderr());
+    }
+
+    dispatch.apply()?;
+    Ok(())
 }
