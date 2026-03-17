2d7e3d8b4398e5aff73b1b1755760a9b251f0520
wldmr <nope@wldmr.net>
2026-03-17 16:57:31 +0100

    Add diagnostics for illegal targets/duplicate definitions

--- src/lsp/salsa/subqueries/diagnostics.rs
+++ src/lsp/salsa/subqueries/diagnostics.rs
@@ -1,5 +1,5 @@
 use ink_document::InkDocument;
-use lsp_types::{Diagnostic, DiagnosticSeverity};
+use lsp_types::{Diagnostic, DiagnosticRelatedInformation, DiagnosticSeverity, Location};
 use mini_milc::{subquery, Db};
 
 use crate::lsp::{
@@ -17,12 +17,12 @@ subquery!(Ops, file_diagnostics, FileDiagnostics, |self, db| {
     let doc = db.document(self.docid);
     let node_infos = db.node_infos(self.docid);
     let mut errors = parse_errors(&doc);
-    find_unused(&mut errors, db, &doc, self.docid, &node_infos);
-    find_unresolved(&mut errors, db, &doc, self.docid, &node_infos);
+    add_unused(&mut errors, db, &doc, self.docid, &node_infos);
+    add_illegal_targets(&mut errors, db, &doc, self.docid, &node_infos);
     errors
 });
 
-fn find_unused(
+fn add_unused(
     diags: &mut FileDiagnostics,
     db: &impl Db<Ops>,
     doc: &InkDocument,
@@ -48,7 +48,7 @@ fn find_unused(
                 Const => "constant",
                 List => "list",
                 ListItem => "list item",
-                _ => "unknown kind of definition (this is a bug)",
+                _ => "unknown kind of definition (This is likely a bug in ink-tool.)",
             });
             let name = doc.text(doc.byte_range(range.into()));
             diags.push(Diagnostic {
@@ -61,7 +61,7 @@ fn find_unused(
     }
 }
 
-fn find_unresolved(
+fn add_illegal_targets(
     diags: &mut FileDiagnostics,
     db: &impl Db<Ops>,
     doc: &InkDocument,
@@ -70,22 +70,101 @@ fn find_unresolved(
 ) {
     use NodeFlag::*;
 
-    let usages = node_infos
-        .iter_flags()
-        .filter(|(_range, flags)| flags.contains(Usage) && !flags.contains(Definition));
+    let usages = node_infos.iter_flags().filter(|(_range, flags)| {
+        flags.intersects(Usage) && !flags.intersects(Definition | Builtin)
+    });
 
+    let uris = db.doc_ids();
     for (usage, flags) in usages {
-        if db.definition_of(docid, usage).is_empty() {
-            let name = doc.text(doc.byte_range(usage.into()));
-            if flags.contains(Redirect) && (name == "DONE" || name == "END") {
-                continue; // These are special, they're never unresolved
-            }
+        let definition = db.definition_of(docid, usage);
+        let text = doc.text(doc.byte_range(usage.into()));
+
+        if definition.is_empty() {
+            let kind = match_flags!(match (flags) {
+                Redirect => "location",
+                Call => "function",
+                _ => "name",
+            });
             diags.push(Diagnostic {
                 range: usage.into(),
                 severity: Some(DiagnosticSeverity::ERROR),
-                message: format!(r#"Undefined name "{name}""#),
+                message: format!(r#"Undefined {kind} "{text}""#),
                 ..Default::default()
             });
+        } else {
+            let mut ambiguous_targets = Vec::new();
+            let mut illegal_targets = Vec::new();
+
+            for (def_doc, def_range) in definition.iter().copied() {
+                let def_flags = db.node_infos(def_doc).flags(*def_range);
+
+                let def_kind = match_flags!(match (def_flags) {
+                    Function => "function",
+                    Knot => "knot",
+                    Stitch => "stitch",
+                    Label => "label",
+                    Temp => "temporary variable",
+                    Param => "parameter",
+                    Var => "variable",
+                    Const => "constant",
+                    List => "list",
+                    ListItem => "list item",
+                    _ => "unknown kind of definition (this is a bug)",
+                });
+
+                ambiguous_targets.push(DiagnosticRelatedInformation {
+                    location: Location::new(uris[def_doc].clone(), def_range.into()),
+                    message: format!("{def_kind} defined here"),
+                });
+
+                if flags.contains(Redirect) {
+                    if !def_flags.intersects(Knot | Stitch | Label | Var | Param | Temp) {
+                        // Var’s, Temps, Params *might* contain an address. We could get more granular
+                        // here, but this would get us in the weeds of infering types in a dynamically
+                        // typed language.
+                        illegal_targets.push(DiagnosticRelatedInformation {
+                            location: Location::new(uris[def_doc].clone(), def_range.into()),
+                            message: format!("a {def_kind} is not an address"),
+                        });
+                    }
+                }
+
+                if flags.contains(Call) {
+                    if !def_flags.intersects(Function | HasParams) {
+                        illegal_targets.push(DiagnosticRelatedInformation {
+                            location: Location::new(uris[def_doc].clone(), def_range.into()),
+                            message: format!("a {def_kind} can not be called"),
+                        });
+                    }
+                }
+            }
+
+            if ambiguous_targets.len() >= 2 {
+                diags.push(Diagnostic {
+                    range: usage.into(),
+                    severity: Some(DiagnosticSeverity::ERROR),
+                    message: format!(r#"Multiple definitions for "{text}""#),
+                    related_information: Some(ambiguous_targets),
+                    ..Default::default()
+                });
+            }
+
+            if illegal_targets.len() != 0 {
+                diags.push(Diagnostic {
+                    range: usage.into(),
+                    severity: Some(DiagnosticSeverity::ERROR),
+                    message: match_flags!(match (flags) {
+                        Redirect => format!(r#"Can not redirect to {text}"#),
+                        Call => format!(r#"Can not call {text}"#),
+                        _ => format! {r#"Some problem with "{text}"
+
+(This message should be more specific. You've likely found a bug in ink-tool)"#
+                        },
+                    }),
+                    related_information: Some(illegal_targets),
+                    ..Default::default()
+                });
+            }
         }
     }
 }
