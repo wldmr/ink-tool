93c518593198d34f0e196374514923f596f92635
wldmr <nope@wldmr.net>
2026-03-25 13:05:09 +0100

    Refactor: Make `fill_transitive` a method

--- src/lsp/salsa/subqueries/story_structure.rs
+++ src/lsp/salsa/subqueries/story_structure.rs
@@ -44,52 +44,49 @@ impl Subquery<Ops, TransitiveImports> for transitive_imports {
             .collect();
 
         let mut result = TransitiveImports::default();
-        fill_transitive(&mut result, db, &ids, root_dir, self.root.0);
+        result.fill_transitive(db, &ids, root_dir, self.root.0);
         old.update(result)
     }
 }
 
-fn fill_transitive(
-    imported: &mut TransitiveImports,
-    db: &impl Db<Ops>,
-    ids: &HashMap<&Path, Id<Uri>>,
-    root_dir: &Path,
-    current_file: DocId,
-) {
-    imported
-        .resolved
-        .entry(current_file)
-        .and_modify(|vec1| vec1.push(FileTextRange::start_of(current_file)))
-        .or_insert_with(|| Vec1::new(FileTextRange::start_of(current_file)));
+impl TransitiveImports {
+    fn fill_transitive(
+        &mut self,
+        db: &impl Db<Ops>,
+        ids: &HashMap<&Path, Id<Uri>>,
+        root_dir: &Path,
+        current_file: DocId,
+    ) {
+        self.resolved
+            .entry(current_file)
+            .and_modify(|vec1| vec1.push(FileTextRange::start_of(current_file)))
+            .or_insert_with(|| Vec1::new(FileTextRange::start_of(current_file)));
 
-    let doc = db.document(current_file);
-    let infos = db.node_infos(current_file);
+        let doc = db.document(current_file);
+        let infos = db.node_infos(current_file);
 
-    for range in infos.imported_files() {
-        let import = doc.lsp_text(range);
-        let path = root_dir.join(import);
-        if let Some(resolved) = ids.get(path.as_path()).copied() {
-            let import_location = FileTextRange::new(current_file, range);
-            let imports = imported
-                .resolved
-                .entry(resolved)
-                .and_modify(|vec1| vec1.push(import_location))
-                .or_insert_with(|| Vec1::new(import_location));
-            if imports.len() > 1 {
-                let uris = db.doc_ids();
-                let resolved_uri = uris[resolved].path().as_str();
-                let source_uri = uris[import_location.file].path().as_str();
-                let source_range = import_location.range;
-                log::warn!("{source_uri}:{source_range} : Duplicate (or circular) import of {resolved_uri}");
+        for range in infos.imported_files() {
+            let import = doc.lsp_text(range);
+            let path = root_dir.join(import);
+            if let Some(resolved) = ids.get(path.as_path()).copied() {
+                let import_location = FileTextRange::new(current_file, range);
+                let imports = self
+                    .resolved
+                    .entry(resolved)
+                    .and_modify(|vec1| vec1.push(import_location))
+                    .or_insert_with(|| Vec1::new(import_location));
+                if imports.len() > 1 {
+                    let uris = db.doc_ids();
+                    let resolved_uri = uris[resolved].path().as_str();
+                    let source_uri = uris[import_location.file].path().as_str();
+                    let source_range = import_location.range;
+                    log::warn!("{source_uri}:{source_range} : Duplicate (or circular) import of {resolved_uri}");
+                } else {
+                    self.fill_transitive(db, ids, root_dir, resolved);
+                }
             } else {
-                fill_transitive(imported, db, ids, root_dir, resolved);
+                self.unresolved.entry(current_file).or_default().push(range);
             }
-        } else {
-            imported
-                .unresolved
-                .entry(current_file)
-                .or_default()
-                .push(range);
         }
     }
 }
